7180c04238ed216ba938658b4046a61f6da8359b
Sankalp <sanklpkaushik21@gmail.com>
2024-03-08 19:43:25 +0530

    Adding Stop Text Selection

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en" className="scroll-smooth select-none">
       <body className={`${inter.className} bg-background`}>
         <NavBar />
         {children}
